74fd92e413e770a76c705aff854d75e7e3098ab9
John Cokos <highlander.44@gmail.com>
2021-12-21 20:54:02 -0800

    Website Demo

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let name = "john";
+let isCorrect = false;
+let guess = "";
+
+while (!isCorrect) {
+  guess = prompt("What is the teachers name?");
+  if (guess === name) {
+    isCorrect = true;
+  }
+}
+
+document.write("You got it!");
+
+let numGuesses = 4;
+let numCats = 0;
+
+for (let i = 1; i <= numGuesses; i++) {
+  let catsGuess = prompt("How many cats do I have?");
+  // == lets numbers and letters be checked for equality...
+  // === means they must be equal and of same type (num/string)
+  if (catsGuess == numCats) {
+    document.write("you guessed right!");
+    break;
+  }
+}
+
+// Why doesn't guess show up? We got it right????
+// document.write(guess);
